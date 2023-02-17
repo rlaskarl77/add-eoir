45f50531470a779837ba83cd3f0dd85f68edaf37
nam <rlaskarl77@gmail.com>
2023-02-17 10:51:29 +0900

    conda requirements edited

--- scripts/train_multispectral_cpp_kaist.sh
+++ scripts/train_multispectral_cpp_kaist.sh
@@ -1,14 +1,14 @@
 nohup python -um torch.distributed.run --nproc_per_node 4 train_multispectral.py \
     --device 0,1,2,3 \
-    --data /home/namgi/yolov5_latest/data/multispectral/add-eoir-test-segment.yaml \
+    --data data/multispectral/kaist.yaml \
     --weights yolov5l.pt \
-    --hyp data/hyps/hyp.add-eoir-cpp.yaml \
-    --batch-size 64 \
-    --person-only \
+    --hyp data/hyps/hyp.kaist-multispectral.yaml \
+    --batch-size 48 \
     --exist-ok \
-    --epochs 100 \
+    --cache ram \
+    --epochs 200 \
     --entity cvlabnam \
-    --project copy_paste \
-    --name v2.0_Multispectral_copypaste@0.5_sizedist_posVar_100epoch \
-    > logs/train_copy_paste/v2.0_train_Multispectral_copypaste@0.5_sizedist_posVar_100epoch.log 2>&1 &
+    --project /131_data/namgi/add_eoir/checkpoints/kaist \
+    --name local_multispectral_copypaste@0.5_ellipse_200epochs \
+    > /131_data/namgi/add_eoir/logs/local_multispectral_copypaste@0.5_ellipse_200epochs.log 2>&1 &
 disown -a
