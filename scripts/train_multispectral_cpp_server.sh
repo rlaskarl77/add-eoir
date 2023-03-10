b4059a98c7532cfadd23994e60701f3136227267
nam <rlaskarl77@gmail.com>
2023-03-10 23:48:43 +0900

    8gpu server training bash code added

--- scripts/train_multispectral_cpp_server.sh
+++ scripts/train_multispectral_cpp_server.sh
@@ -1,15 +1,15 @@
 EPOCH=150
-python -um torch.distributed.run --nproc_per_node 4 train_multispectral_add_eoir.py \
-    --device 0,1,2,3 \
+python -um torch.distributed.run --nproc_per_node 8 train_multispectral_add_eoir.py \
+    --device 0,1,2,3,4,5,6,7 \
     --data data/multispectral/add-eoir-test-segment-server.yaml \
     --weights yolov5l.pt \
     --hyp data/hyps/hyp.add-eoir-cpp.yaml \
-    --batch-size 128 \
+    --batch-size 256 \
     --person-only \
     --exist-ok \
-    --cache disk \
+    --cache ram \
     --epochs $EPOCH \
     --entity cvlab_detection \
     --project /131_data/namgi/add_eoir/checkpoints/copy_paste \
-    --name v2.1_Multispectral_bs64_midsize_noocclusion_originaldist_copypaste@1.0_${EPOCH}epoch \
-    > /131_data/namgi/add_eoir/logs/copy_paste/v2.1_Multispectral_bs64_midsize_noocclusion_originaldist_copypaste@1.0_${EPOCH}epoch.log 2>&1
+    --name v2.1_Multispectral_bs256_midsize_noocclusion_originaldist_copypaste@1.0_${EPOCH}epoch \
+    > /131_data/namgi/add_eoir/logs/copy_paste/v2.1_Multispectral_bs256_midsize_noocclusion_originaldist_copypaste@1.0_${EPOCH}epoch.log 2>&1
